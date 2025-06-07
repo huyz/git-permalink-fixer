8aa049131a0f12344941f9289830d1b176d023a9
huyz <huyz@users.noreply.github.com>
2025-06-07 03:05:40 -0700

    🚧 Reorganize files per rye convention

--- src/git_permalink_checker/__init__.py
+++ src/git_permalink_checker/__init__.py
@@ -1685,5 +1685,7 @@ def main():
         traceback.print_exc(file=sys.stderr)
         sys.exit(1)
 
+    return 0
+
 if __name__ == "__main__":
     main()
---
+++ src/git_permalink_checker/__main__.py
@@ -0,0 +1,4 @@
+import git_permalink_checker
+import sys
+
+sys.exit(git_permalink_checker.main())
